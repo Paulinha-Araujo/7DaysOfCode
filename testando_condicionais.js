44b795faa64188fb201651c1fbe7d5a5cc24d413
Ana Paula <ana.paulinhaufrn@gmail.com>
2023-07-06 09:16:18 -0300

    1º dia do DayOfCode da Alura

---
+++ testando_condicionais.js
@@ -0,0 +1,24 @@
+let nUm = 1;
+let stringUm = "1";
+let nTrinta = 30;
+let stringTrinta = "30";
+let nDez = 10;
+let stringDez = "10";
+
+if (nUm == stringUm) {
+    console.log("As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes")
+} else {
+    console.log("As variáveis númeroUm e StringUm tem o mesmo valor")
+}
+
+if (nTrinta === stringTrinta) {
+    console.log("As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo")
+} else {
+    console.log("As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo")
+}
+
+if (nDez == stringDez) {
+    console.log("As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes")
+} else {
+    console.log("As variáveis numeroDez e stringDez não tem o mesmo valor")
+}
